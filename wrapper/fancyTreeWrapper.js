a3673ea2f943c3ec8822f58a461dd68aa61952ce
fmaieli <maieli.franco@gmail.com>
2021-05-13 17:33:49 -0300

    deleted unnecessary functions inside FancyTreeWrapper

--- wrapper/fancyTreeWrapper.js
+++ wrapper/fancyTreeWrapper.js
@@ -1,12 +1,6 @@
 var $ = require('jquery');
 
 export default class FancyTreeWrapper {
-  constructor(_idComponente) {
-    this.idComponente = _idComponente;
-  }
-
-  callTiposEnsayos(empresa, anio, generacion) {}
-
   mapTiposEnsayos(data) {
     var self = this;
     var arrayResult = [];
@@ -40,51 +34,5 @@ export default class FancyTreeWrapper {
     };
 
     return newOption;
-  }
-
-  createTree(data) {
-    $('#' + this.idComponente).fancytree({
-      checkbox: true,
-      icons: false,
-      selectMode: 3,
-      source: data,
-      beforeSelect: function(event, data) {
-        $('#Nombre').prop('disabled', false);
-        $('#GID').prop('disabled', false);
-        $('#btnAgregar').prop('disabled', false);
-      }
-    });
-  }
-
-  selectedElements() {
-    var tree = $('#' + this.idComponente)
-      .fancytree('getTree')
-      .getSelectedNodes();
-    var arraySelectedElements = [];
-    var y = 0;
-    var x = 0;
-
-    for (x = 0; x < tree.length; x++) {
-      if (tree[x].refKey == '1') {
-        arraySelectedElements[y] = tree[x].key;
-        y++;
-      }
-    }
-
-    return arraySelectedElements;
-  }
-
-  getLocalidadesSeleccionadas() {
-    var localidadesSeleccionadas = [];
-
-    $('#' + this.idComponente)
-      .fancytree('getRootNode')
-      .visit(function(node) {
-        if (!node.hasChildren() && node.selected === true) {
-          localidadesSeleccionadas.push(node);
-        }
-      });
-
-    return localidadesSeleccionadas;
   }
 }
